d8b71fa7f04929dd2085641ffe80cefd70f03232
Maria Buznik <masha.vinogradova82@gmail.com>
2022-11-01 21:17:20 +0200

    Summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,94 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Form from './Form';
 import ContactsList from './ContactsList';
 import Filter from './Filter';
 
 const CONTACTS_LOCALSTORAGE_KEY = 'contacts';
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const inContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-   componentDidMount() {
-    const parsedContacts = JSON.parse(
-      localStorage.getItem(CONTACTS_LOCALSTORAGE_KEY)
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem(CONTACTS_LOCALSTORAGE_KEY)) ?? inContacts
     );
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  });
 
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem(CONTACTS_LOCALSTORAGE_KEY, JSON.stringify(contacts));
-    }
-  }
+  const [filter, stFilter] = useState('');
 
-  handleFormSubmit = profile => {
-    const { contacts } = this.state;
+  const handleFormSubmit = profile => {
     const nameToCheck = profile.name.toLocaleLowerCase();
     const isIncludeName = contacts.some(
       contact => contact.name.toLocaleLowerCase() === nameToCheck
     );
     if (isIncludeName) {
-      alert(`${profile.name} is already in contacts`);
+      toast.warn(`${profile.name} is already in contacts`, { autoClose: 2000 });
       return;
     }
-    this.setState(({ contacts }) => {
-      return {
-        contacts: [profile, ...contacts],
-      };
-    });
+    setContacts(prevState => [profile, ...prevState]);
   };
 
-  handleFilterChange = evt => {
+  const handleFilterChange = evt => {
     const { value } = evt.currentTarget;
-    this.setState({ filter: value });
+    stFilter(value);
   };
 
-  handleDeleteContact = id => {
-    const { contacts } = this.state;
-    const updatedContacts = contacts.filter(contact => contact.id !== id);
-    this.setState({ contacts: updatedContacts });
-  };
-
-  getFilteredContacts = () => {
-    const { filter, contacts } = this.state;
+  const filteredContacts = useMemo(() => {
     const normalizedContacts = filter.toLocaleLowerCase();
     return contacts.filter(({ name }) =>
       name.toLocaleLowerCase().includes(normalizedContacts)
     );
+  }, [contacts, filter]);
+
+  const handleDeleteContact = id => {
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    setContacts(updatedContacts);
   };
 
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFilteredContacts();
+  useEffect(() => {
+    localStorage.setItem(CONTACTS_LOCALSTORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
-    return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          fontSize: 30,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <Form onFormSubmit={this.handleFormSubmit} />
-        <h2 className="contacts__title">Contacts</h2>
-        <Filter value={filter} onChange={this.handleFilterChange} />
-        <ContactsList
-          contacts={filteredContacts}
-          onDelete={this.handleDeleteContact}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        fontSize: 30,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <Form onFormSubmit={handleFormSubmit} />
+      <h2 className="contacts__title">Contacts</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactsList
+        contacts={filteredContacts}
+        onDelete={handleDeleteContact}
+      />
+      <ToastContainer />
+    </div>
+  );
+};
